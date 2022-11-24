584ac96e529edb635f2e6de39c3e7b7d29a86f55
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-11-24 11:07:37 +0100

    Start creating a Hydra provider by mocking

--- hydraw/paint.js
+++ hydraw/paint.js
@@ -1,8 +1,29 @@
 import { Lucid } from "https://unpkg.com/lucid-cardano@0.7.8/web/mod.js"
 
+const HydraProvider = {
+  getProtocolParameters: () => {
+    console.log("getProtocolParameters");
+    return {
+      minFeeA: 0, minFeeB: 0, maxTxSize: 0, maxValSize: 0,
+      keyDeposit: BigInt(0),
+      poolDeposit: BigInt(0),
+      priceMem: 0, priceStep: 0,
+      maxTxExMem: BigInt(0),
+      maxTxExSteps: BigInt(0),
+      coinsPerUtxoByte: BigInt(0),
+      collateralPercentage: 0,
+      maxCollateralInputs: 0,
+      costModels: {
+        PlutusV1: [],
+        PlutusV2: []
+      }
+    }
+  }
+}
 
 export const paintPixel = async (x, y, color) => {
   console.log("should paint pixel: ", x, y, color);
-  const lucid = await Lucid.new({}, "Hydra");
+  // NOTE: The network is only used for slot config / time stuff.
+  const lucid = await Lucid.new(HydraProvider, "Preview");
   console.log(lucid);
 }
