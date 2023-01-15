d447e31497dab640e41d053957b8796e62b14f06
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-01-15 22:26:42 +0100

    Replace websocket lib with custom ws effect
    The library 'loses' messages from the server...

--- hydra-explorer/ui/src/App.js
+++ hydra-explorer/ui/src/App.js
@@ -1,46 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useCallback, useEffect } from 'react';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
 //const baseUrl = protocol + "//" + window.location.host + window.location.pathname;
 const baseUrl = 'ws://explorer.hydra.family';
 
 const initialState = {
-  'lastSlot': 0,
-  'countTxs': 0,
-  'lastBlock': ''
+  lastSlot: 7126228,
+  countTxs: 0,
+  lastBlock: '',
+  heads: []
 };
 
-function Stats(props) {
+function Stats({ lastSlot, lastBlock, countTxs }) {
   return <div className='stats'>
     <h2>Basic stats</h2>
-    <div>Slot: <span className='slot'>{props.lastSlot}</span></div>
-    <div>Block: <span className='block'>{props.lastBlock}</span></div>
-    <div>Txs: <span className='txs'>{props.countTxs}</span></div>
+    <div className='stat'><label>Slot</label><span>{lastSlot}</span></div>
+    <div className='stat'><label>Block</label>
+      <span>
+        <a href={'https://preview.cexplorer.io/block/' + lastBlock}>{lastBlock.substring(0, 30) + '...'}</a>
+      </span>
+    </div>
+    <div className='stat'><label>Txs</label><span>{countTxs}</span></div>
   </div>;
 }
 
-function App() {
-  //Public API that will echo messages sent to it back to the client
-  const [socketUrl, setSocketUrl] = useState(baseUrl + '/');
-  const [heads, setHeads] = useState(initialState);
+function HeadId({ headId }) {
+  return <div className='headId'><label>HeadId</label><span>{headId}</span></div>;
+}
 
-  const { lastJsonMessage, readyState } = useWebSocket(socketUrl);
+function Head(head) {
+  return <div className="head">
+    <HeadId headId={head.headId} />
+  </div>;
+}
+
+/** Display the list of current heads along with their status
+*/
+function Heads({ heads }) {
+  return <div className='heads'>
+    {heads.map(Head)}
+  </div>;
+}
+
+const SOCKET_RECONNECTION_TIMEOUT = 30;
+
+const webSocket = new WebSocket(baseUrl);
+
+function App() {
+  const [ws, setWs] = useState(webSocket);
+  const [socketUrl, setSocketUrl] = useState(baseUrl + '/' + initialState.lastSlot);
+  const [heads, setHeads] = useState(initialState);
 
   function updateState(msg) {
     return (state) => {
       switch (msg.tag) {
-        // case "HeadInit":
-        //   setTimeout(() => displayInit(msg), 0);
+        case 'HeadInit':
+          return {
+            ...state,
+            heads: [{ headId: msg.headInit.headId }, ...state.heads]
+          };
+
 
         // case "HeadCommit":
         //   setTimeout(() => displayCommit(msg), 0);
 
-        case "Forward":
+        case 'Forward':
           return {
-            ...state, countTxs: state.countTxs + 1, lastSlot: msg.point.slot, lastBlock: msg.point.blockHash
+            ...state,
+            countTxs: state.countTxs + 1,
+            lastSlot: msg.point.slot,
+            lastBlock: msg.point.blockHash
           };
         default:
           console.log("irrelevant message", msg);
@@ -50,34 +81,41 @@ function App() {
   }
 
   useEffect(() => {
-    if (lastJsonMessage !== null) {
-      setHeads(updateState(lastJsonMessage));
-    }
-  }, [lastJsonMessage, setHeads]);
+    const onClose = () => {
+      setTimeout(() => {
+        setWs(new WebSocket(socketUrl));
+      }, SOCKET_RECONNECTION_TIMEOUT);
+    };
+
+    const onMessage = (e) => {
+      const msg = JSON.parse(e.data);
+      setHeads(updateState(msg));
+    };
+
+    ws.addEventListener("close", onClose);
+    ws.addEventListener("message", onMessage);
+
+    return () => {
+      ws.removeEventListener("close", onClose);
+      ws.removeEventListener("message", onMessage);
+    };
+  }, [ws, setWs, socketUrl, setSocketUrl]);
 
   // const handleClickChangeSocketUrl = useCallback(
   //   () => setSocketUrl(baseUrl + '/' + lastSlot),
   //   [lastSlot]
   // );
 
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-  }[readyState];
-
   return (
     <div className="App">
       <header className="App-header">
         <img alt="Hydra Logo" width="100" src="/logo.png"></img>
         <h1>Hydra Observer</h1>
-        <span>The WebSocket is currently {connectionStatus}</span>
         <Stats lastSlot={heads.lastSlot}
           lastBlock={heads.lastBlock}
           countTxs={heads.countTxs}
         />
+        <Heads heads={heads.heads} />
       </header>
     </div >
   );
